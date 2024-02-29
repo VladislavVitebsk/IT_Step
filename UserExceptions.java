d81927430f6288ad68a2fc62814fe26bc325fc07
Vladislav <159304133+VladislavVitebsk@users.noreply.github.com>
2024-02-29 21:32:09 +0300

    Add files via upload

---
+++ UserExceptions.java
@@ -0,0 +1,35 @@
+package exception;
+
+public class UserExceptions {
+    public static void main(String[] args) {
+        int a= 1;
+        //if (a%2==1) throw new NotEvenException("not even");
+        new Person("person");
+    }
+}
+
+class NotEvenException extends Exception{
+    public NotEvenException(String s){
+        super(s);
+    }
+}
+class Person{
+    private String name = "";
+    Person(String name) throws NotCapitalLetterException{
+        this.name = name;
+        validateName();
+    }
+
+    private void validateName() throws NotCapitalLetterException{
+        if (Character.isLowerCase(name.charAt(0))) {
+            throw new NotCapitalLetterException("The first letter of name "+name+ " must be capital");
+        }
+    }
+}
+
+class NotCapitalLetterException extends Exception {
+
+    NotCapitalLetterException(String str) {
+        super(str);
+    }
+}
