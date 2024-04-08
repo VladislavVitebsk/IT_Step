21aa0e65daca4e3b70465530fedb51b2324822ea
Vladislav <159304133+VladislavVitebsk@users.noreply.github.com>
2024-04-08 19:57:57 +0300

    Update homework11_03_до0804_SequenceGenerator.java

--- homework11_03_до0804_SequenceGenerator.java
+++ homework11_03_до0804_SequenceGenerator.java
@@ -1,64 +1,106 @@
 package HomeWork_20240311_20240408;
+import java.util.Scanner;
 
-import java.util.ArrayList;
-import java.util.List;
+interface Sequence {
+    void generate(int n);
+}
 
-//Шаг 4: Использование декоратора для измерения времени работы алгоритмов.
+class FibonacciSequence implements Sequence {
+    @Override
+    public void generate(int n) {
+        long startTime = System.nanoTime();
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        long endTime = System.nanoTime();
+        System.out.println("\nTime taken to generate Fibonacci Sequence: " + (endTime - startTime) + " nanoseconds");
+    }
+}
+
+class PrimeSequence implements Sequence {
+    @Override
+    public void generate(int n) {
+        long startTime = System.nanoTime();
+        int count = 0;
+        for (int i = 2; count < n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("\nTime taken to generate Prime Sequence: " + (endTime - startTime) + " nanoseconds");
+    }
+}
+
+class FactorialSequence implements Sequence {
+    @Override
+    public void generate(int n) {
+        long startTime = System.nanoTime();
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            System.out.print(fact + " ");
+        }
+        long endTime = System.nanoTime();
+        System.out.println("\nTime taken to generate Factorial Sequence: " + (endTime - startTime) + " nanoseconds");
+    }
+}
+
+class SequenceGeneratorDecorator implements Sequence {
+    private Sequence sequence;
+
+    public SequenceGeneratorDecorator(Sequence sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public void generate(int n) {
+        sequence.generate(n);
+    }
+}
+
 public class SequenceGenerator {
     public static void main(String[] args) {
-        NumberSequenceGenerator fibonacciGenerator = new FibonacciSequenceGenerator();
-        NumberSequenceGenerator primeNumberGenerator = new PrimeNumberSequenceGenerator();
-        NumberSequenceGenerator factorialGenerator = new FactorialSequenceGenerator();
+        Scanner scanner = new Scanner(System.in);
 
-        NumberSequenceGenerator timedFibonacciGenerator = new TimeDecorator(fibonacciGenerator);
-        NumberSequenceGenerator timedPrimeNumberGenerator = new TimeDecorator(primeNumberGenerator);
-        NumberSequenceGenerator timedFactorialGenerator = new TimeDecorator(factorialGenerator);
+        System.out.println("Enter the type of sequence to generate:");
+        System.out.println("1 - Fibonacci Sequence");
+        System.out.println("2 - Prime Sequence");
+        System.out.println("3 - Factorial Sequence");
+        int choice = scanner.nextInt();
 
-        List<Integer> fibonacciSequence = timedFibonacciGenerator.generateSequence(10);
-        List<Integer> primeNumberSequence = timedPrimeNumberGenerator.generateSequence(10);
-        List<Integer> factorialSequence = timedFactorialGenerator.generateSequence(10);
-    }
-}
-//Шаг 1: Создание интерфейса для генератора последовательностей чисел.
-public interface NumberSequenceGenerator {
-    List<Integer> generateSequence(int n);
-}
-//Шаг 2: Создание классов для конкретных последовательностей чисел - Фибоначчи, простых чисел, факториалов.
-public class FibonacciSequenceGenerator implements NumberSequenceGenerator {
-    @Override
-    public List<Integer> generateSequence(int n) {
-        List<Integer> sequence = new ArrayList<>();// генерация последовательности чисел Фибоначчи
-        return sequence;
-    }
-}
-public class PrimeNumberSequenceGenerator implements NumberSequenceGenerator {
-    @Override
-    public List<Integer> generateSequence(int n) {
-        List<Integer> sequence = new ArrayList<>();// генерация последовательности простых чисел
-        return sequence;
-    }
-}
-public class FactorialSequenceGenerator implements NumberSequenceGenerator {
-    @Override
-    public List<Integer> generateSequence(int n) {
-        List<Integer> sequence = new ArrayList<>();// генерация последовательности факториалов чисел
-        return sequence;
-    }
-}
-//Шаг 3: Создание класса-декоратора для измерения времени работы алгоритма.
-public class TimeDecorator implements NumberSequenceGenerator {
-    private NumberSequenceGenerator generator;
+        System.out.println("Enter the number of elements to generate:");
+        int n = scanner.nextInt();
 
-    public TimeDecorator(NumberSequenceGenerator generator) {
-        this.generator = generator;
-    }
+        Sequence sequence = null;
+        switch (choice) {
+            case 1:
+                sequence = new SequenceGeneratorDecorator(new FibonacciSequence());
+                break;
+            case 2:
+                sequence = new SequenceGeneratorDecorator(new PrimeSequence());
+                break;
+            case 3:
+                sequence = new SequenceGeneratorDecorator(new FactorialSequence());
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
 
-    @Override
-    public List<Integer> generateSequence(int n) {
-        long startTime = System.nanoTime();
-        List<Integer> sequence = generator.generateSequence(n);
-        long endTime = System.nanoTime();
-        System.out.println("Time taken: " + (endTime - startTime) + " nanoseconds");
-        return sequence;
+        if (sequence != null) {
+            sequence.generate(n);
+        }
     }
 }
